60a50884214022312a0d9734697956a70849edb5
Drwalin <marek.zalewski.drwalin@gmail.com>
2023-02-20 19:42:19 +0100

    Some initial class interfaces.

---
+++ include/quickgl/AllocatorVBO.hpp
@@ -0,0 +1,49 @@
+/*
+ *  This file is part of QuickGL.
+ *  Copyright (C) 2023 Marek Zalewski aka Drwalin
+ *
+ *  QuickGL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QuickGL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef QUICKGL_ALLOCATOR_VBO_H
+#define QUICKGL_ALLOCATOR_VBO_H
+
+#include <memory>
+#include <vector>
+#include <map>
+
+#include "../../OpenGLWrapper/include/openglwrapper/VBO.hpp"
+
+namespace qgl {
+	class AllocatorVBO {
+	public:
+		
+		AllocatorVBO(uint32_t elementSize, bool isElementOtherwiseVertexBuffer);
+		~AllocatorVBO();
+		
+		uint32_t Allocate(uint32_t sizeElements);
+		void Free(uint32_t posElements, uint32_t sizeElements);
+		
+		inline gl::VBO& VBO() { return vbo; }
+		
+	private:
+		
+		std::map<uint32_t, uint32_t> freeRanges;
+		
+		gl::VBO vbo;
+	};
+}
+
+#endif
+
---
+++ include/quickgl/MeshCategoryRenderer.hpp
@@ -0,0 +1,56 @@
+/*
+ *  This file is part of QuickGL.
+ *  Copyright (C) 2023 Marek Zalewski aka Drwalin
+ *
+ *  QuickGL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QuickGL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef QUICKGL_MESH_CATEGORY_RENDERER_HPP
+#define QUICKGL_MESH_CATEGORY_RENDERER_HPP
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+#include "../../OpenGLWrapper/include/openglwrapper/FBO.hpp"
+#include "../../OpenGLWrapper/include/openglwrapper/VBO.hpp"
+#include "../../OpenGLWrapper/include/openglwrapper/VAO.hpp"
+#include "../../OpenGLWrapper/include/openglwrapper/Shader.hpp"
+#include "../../OpenGLWrapper/include/openglwrapper/Texture.hpp"
+#include "../../OpenGLWrapper/include/openglwrapper/OpenGL.hpp"
+
+#include "AllocatorVBO.hpp"
+#include "MeshManager.hpp"
+
+namespace qgl {
+	
+	class MeshCategoryRenderer {
+	public:
+		
+		MeshCategoryRenderer();
+		~MeshCategoryRenderer();
+		
+	private:
+		
+		MeshManager meshManager;
+		
+		gl::VBO instanceData;
+		gl::VBO indirectDrawBuffer;
+	};
+}
+
+#endif
+
