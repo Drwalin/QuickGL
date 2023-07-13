f2e63200d7df9e1be048630377425bb686caec23
Drwalin <34726800+Drwalin@users.noreply.github.com>
2023-07-13 13:36:27 +0200

    Written classes declaration for DeltaVboManager and MoveVboUpdater/MoveVboUpdaterManager

---
+++ include/quickgl/util/MoveVboUpdater.hpp
@@ -0,0 +1,82 @@
+/*
+ *  This file is part of QuickGL.
+ *  Copyright (C) 2023 Marek Zalewski aka Drwalin
+ *
+ *  QuickGL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QuickGL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef QUICKGL_MOVE_VBO_UPDATER_HPP
+#define QUICKGL_MOVE_VBO_UPDATER_HPP
+
+#include <cinttypes>
+
+#include <memory>
+#include <unordered_map>
+
+namespace gl {
+#ifndef OGLW_VBO_HPP
+	class VBO;
+#endif
+#ifndef OGLW_Shader_HPP
+	class Shader;
+#endif
+}
+
+namespace cql {
+	class MoveVboUpdater final {
+	public:
+		
+		struct PairMove {
+			uint32_t from;
+			uint32_t to;
+		};
+		
+		MoveVboUpdater(uint32_t bytes);
+		~MoveVboUpdater();
+		
+		void Init();
+		
+		void Update(gl::VBO* vbo, const PairMove* data, uint32_t elements);
+		
+		friend class MoveVboManager;
+		
+	private:
+		
+		gl::Shader* shader;
+		const uint32_t BYTES;
+	};
+	
+	class MoveVboManager final {
+	public:
+		
+		MoveVboManager();
+		~MoveVboManager();
+		
+		void Init();
+		void Destroy();
+		
+		std::shared_ptr<MoveVboUpdater> GetByObjectSize(uint32_t bytes);
+		
+		void Update(gl::VBO* vbo, const MoveVboUpdater::PairMove* data,
+				uint32_t elements, uint32_t elementSize);
+		
+	private:
+		
+		std::unordered_map<uint32_t, std::shared_ptr<MoveVboUpdater>> updatersByElementSizeSize;
+		
+	};
+}
+
+#endif
+
