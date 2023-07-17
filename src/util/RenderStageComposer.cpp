375701cd05b038e2fc2a408674ebe8ddadd46190
Drwalin <marek.zalewski.drwalin@gmail.com>
2023-07-17 15:35:09 +0200

    Working on architecture chagne. Does not compile.

--- src/util/RenderStageComposer.cpp
+++ src/util/RenderStageComposer.cpp
@@ -22,290 +22,228 @@
 #include "../../OpenGLWrapper/include/openglwrapper/OpenGL.hpp"
 
 #include "../../include/quickgl/cameras/Camera.hpp"
+#include "../../include/quickgl/pipelines/Pipeline.hpp"
 
 #include "../../include/quickgl/util/RenderStageComposer.hpp"
 
 namespace qgl {
+	void StageTiming::Start(std::shared_ptr<Stage> stage) {
+		this->stage = stage;
+		camera = stage->pipeline->GetStageScheduler().GetCurrentCamera();
+		start = std::chrono::steady_clock::now();
+	}
+	
+	void StageTiming::End() {
+		auto end = std::chrono::steady_clock::now();
+		measuredSeconds =
+				std::chrono::duration_cast<
+					std::chrono::duration<double>>(
+							end - start).count();
+	}
+	
+	
+	
+	
+	void PipelineStagesScheduler::Destroy() {
+		currentCamera = nullptr;
+		globalStages.clear();
+		perCameraStages.clear();
+		renderStageComposer = nullptr;
+	}
+	
+	void PipelineStagesScheduler::AddStage(std::shared_ptr<Stage> stage) {
+		if(stage->executionPolicy < STAGE_CAMERA) {
+			globalStages.push_back(stage);
+		} else {
+			for(int i=0; i<perCameraStages.size(); ++i) {
+				if(perCameraStages[i]->executionPolicy > stage->executionPolicy) {
+					perCameraStages.insert(perCameraStages.begin() + 1, stage);
+					return;
+				}
+			}
+			perCameraStages.push_back(stage);
+		}
+	}
+	
+	bool PipelineStagesScheduler::HasMoreStages() {
+		return GetNextStage() != nullptr;
+	}
+	
+	bool PipelineStagesScheduler::CanExecuteNextStage() {
+		auto stage = GetNextStage();
+		bool ret = stage->canExecute(currentCamera);
+		if(ret && stage->executionPolicy & STAGE_SYNC_AFTER_OTHER_MATERIALS_CURRENT_CAMERA) {
+			return renderStageComposer->CanExecuteSyncStage(currentCameraId,
+					stage->executionPolicy);
+		}
+		return ret;
+	}
+	
+	void PipelineStagesScheduler::ExecuteNextStage() {
+		if(CanExecuteNextStage()) {
+			auto stage = GetNextStage();
+			if(stage->executionPolicy & STAGE_REQUIRE_BOUND_FBO) {
+				stage->pipeline->GetStageScheduler().GetCurrentCamera()->UseFbo();
+			}
+			stage->task(currentCamera);
+			
+			if(nextGlobalStage < globalStages.size()) {
+				++nextGlobalStage;
+				if(nextGlobalStage >= globalStages.size()) {
+					nextPerCameraStage = 0;
+					currentCamera = renderStageComposer
+						->GetCameraByIndex(currentCameraId);
+				}
+			} else if(nextPerCameraStage < perCameraStages.size()) {
+				++nextPerCameraStage;
+				if(nextPerCameraStage >= perCameraStages.size()) {
+					++currentCameraId;
+					nextPerCameraStage = 0;
+					currentCamera = renderStageComposer
+						->GetCameraByIndex(currentCameraId);
+				}
+			}
+		}
+	}
+	
+	void PipelineStagesScheduler::RestartExecution(
+			RenderStageComposer* renderStageComposer) {
+		this->renderStageComposer = renderStageComposer;
+		nextGlobalStage = 0;
+		nextPerCameraStage = 0;
+		currentCameraId = 0;
+		currentCamera = nullptr;
+	}
+	
+	std::shared_ptr<Stage> PipelineStagesScheduler::GetNextStage() {
+		if(nextGlobalStage < globalStages.size()) {
+			return globalStages[nextGlobalStage];
+		} else if(nextPerCameraStage < perCameraStages.size()) {
+			return perCameraStages[nextPerCameraStage];
+		}
+		return nullptr;
+	}
+	
+	
+	
+	
+	
 	
 	RenderStageComposer::RenderStageComposer() {
 		enableGlFinishInEveryStageToProfile = false;
 	}
 	
-	void RenderStageComposer::AddPipelineStages(
-			std::shared_ptr<Pipeline> pipeline) {
-		std::vector<std::shared_ptr<Stage>> renderStagesPtr;
-		{
-			std::vector<Stage> renderStages;
-			pipeline->GenerateRenderStages(renderStages);
-			renderStagesPtr.resize(renderStages.size());
-			for(int i=0; i<renderStages.size(); ++i) {
-				Stage& s = renderStages[i];
-				s.pipeline = pipeline;
-				if(s.canBeContinued == nullptr) {
-					s.canBeContinued = [](std::shared_ptr<Camera>) { return true; };
-				}
-				auto rf = s.renderFunction;
-				auto sn = s.stageName;
-				auto pn = s.pipeline->GetPipelineName();
-				s.renderFunction = [this, rf, sn, pn](std::shared_ptr<Camera> cam){
-					QUICKGL_LOG("start stage  '%s' : '%s'", pn.c_str(), sn.c_str());
-					rf(cam);
-					if(enableGlFinishInEveryStageToProfile) {
+	void RenderStageComposer::AddPipeline(std::shared_ptr<Pipeline> pipeline) {
+		pipelines.push_back(pipeline);
+	}
+	
+	void RenderStageComposer::AddCamera(std::shared_ptr<Camera> camera) {
+		cameras.push_back(camera);
+		RenderAsLast(lastRenderCamera);
+	}
+	
+	void RenderStageComposer::RemoveCamera(std::shared_ptr<Camera> camera) {
+		cameras.erase(std::find(cameras.begin(), cameras.end(), camera));
+	}
+	
+	void RenderStageComposer::RenderAsLast(std::shared_ptr<Camera> camera) {
+		lastRenderCamera = camera;
+		if(camera != nullptr) {
+			RemoveCamera(camera);
+			AddCamera(camera);
+		}
+	}
+	
+	void RenderStageComposer::ResetExecution() {
+		auto start = std::chrono::steady_clock::now();
+		timings.clear();
+		RenderAsLast(lastRenderCamera);
+		for(auto p : pipelines) {
+			p->GetStageScheduler().RestartExecution(this);
+		}
+		hasAnyStagesLeft = true;
+		auto end = std::chrono::steady_clock::now();
+		totalCpuTime =
+				std::chrono::duration_cast<
+					std::chrono::duration<double>>(
+							end - start).count();
+	}
+	/* 
+	 * return false when nothing was updated (no ::canBeContinue() returned
+	 * true.
+	 */
+	bool RenderStageComposer::ContinueStages() {
+		auto start = std::chrono::steady_clock::now();
+		if(this->enableGlFinishInEveryStageToProfile) {
+			gl::Finish();
+		}
+		hasAnyStagesLeft = false;
+		bool executedAny = false;
+		for(auto p : pipelines) {
+			PipelineStagesScheduler& s = p->GetStageScheduler();
+			if(s.HasMoreStages()) {
+				if(s.CanExecuteNextStage()) {
+					timings.emplace_back();
+					timings.back().Start(s.GetNextStage());
+					s.ExecuteNextStage();
+					executedAny = true;
+					if(this->enableGlFinishInEveryStageToProfile) {
 						gl::Finish();
 					}
-					QUICKGL_LOG("finish stage '%s' : '%s'", pn.c_str(), sn.c_str());
-				};
-				renderStagesPtr[i] = std::make_shared<Stage>(std::move(s));
-			}
-		}
-		for(int i=0; i<renderStagesPtr.size(); ++i) {
-			if(i+1 < renderStagesPtr.size()) {
-				renderStagesPtr[i]->nextStage = renderStagesPtr[i+1];
-			}
-		}
-		for(int i=0; i<renderStagesPtr.size(); ++i) {
-			if(renderStagesPtr[i]->stageType != STAGE_GLOBAL) {
-				firstStageForCameraPerPipeline[renderStagesPtr[i]->pipeline]
-					= renderStagesPtr[i];
-			}
-		}
-		stagingPipelineTemplate.emplace_back(renderStagesPtr[0]);
-	}
-	
-	void RenderStageComposer::RestartStages(
-			std::vector<std::shared_ptr<Camera>> cameras) {
-		sumTimeCount = 0;
-		totalTimeCountNs = 0;
-		std::shared_ptr<Camera> cam = nullptr;
-		for(auto camera : cameras) {
-			currentStagesPerCamera.clear();
-			currentStagesPerFbo.clear();
-			camerasRenderingOrder[cam] = camera;
-			cam = camera;
-		}
-		currentStagesGlobal = stagingPipelineTemplate;
-		camerasRenderingOrder[cam] = nullptr;
-		timings.clear();
-	}
-	
-	bool RenderStageComposer::ContinueStages() {
-		auto t1 = std::chrono::steady_clock::now();
-		
-		bool ret = false;
-		ret |= ContinueStagesGlobal();
-		gl::Flush();
-		ret |= ContinueStagesPerCamera();
-		gl::Flush();
-		ret |= ContinueStagesPerFbo();
-		gl::Flush();
-		
-		auto t2 = std::chrono::steady_clock::now();
-		
-		totalTimeCountNs += (t2-t1).count();
-		
-		return ret;
-	}       
-	
-	bool RenderStageComposer::ContinueStagesGlobal() {
-		bool ret = false;
-		std::vector<std::shared_ptr<Stage>> stages;
-		stages.reserve(currentStagesGlobal.size());
-		for(auto s : currentStagesGlobal) {
-			if(s->canBeContinued(nullptr)) {
-				ret = true;
-				auto begin = std::chrono::steady_clock::now();
-				s->renderFunction(nullptr);
-				auto next = s->nextStage;
-				if(next) {
-					switch(next->stageType) {
-					case STAGE_GLOBAL:
-						stages.emplace_back(next);
-						break;
-					case STAGE_PER_CAMERA:
-						currentStagesPerCamera
-							.emplace_back(next, camerasRenderingOrder[nullptr]);
-						break;
-					case STAGE_PER_CAMERA_FBO:
-						currentStagesPerCamera
-							.emplace_back(next, camerasRenderingOrder[nullptr]);
-						break;
-					}
-				}
-				auto end = std::chrono::steady_clock::now();
-				uint64_t t = (end-begin).count();
-				sumTimeCount += t;
-				timings.push_back({s->pipeline, s, nullptr, t});
-			} else {
-				stages.emplace_back(s);
-			}
-		}
-		std::swap(stages, currentStagesGlobal);
-		return ret;
-	}
-	
-	bool RenderStageComposer::ContinueStagesPerCamera() {
-		bool ret = false;
-		std::vector<StageCameraPair> stages;
-		stages.reserve(currentStagesPerCamera.size());
-		for(auto p : currentStagesPerCamera) {
-			if(p.stage->canBeContinued(p.camera)) {
-				ret = true;
-				auto begin = std::chrono::steady_clock::now();
-				p.stage->renderFunction(p.camera);
-				auto next = p.stage->nextStage;
-				if(next) {
-					switch(next->stageType) {
-					case STAGE_GLOBAL:
-						throw std::string("qgl::RenderStageComposer: Cannot run STAGE_GLOBAL after STAGE_PER_CAMERA");
-						break;
-					case STAGE_PER_CAMERA:
-						stages.emplace_back(next, p.camera);
-						break;
-					case STAGE_PER_CAMERA_FBO:
-						stages.emplace_back(next, p.camera);
-						break;
-					}
-				} else {
-					auto c = camerasRenderingOrder[p.camera];
-					if(c != nullptr) {
-						auto s = firstStageForCameraPerPipeline[p.stage->pipeline];
-						if(s != nullptr) {
-							switch(s->stageType) {
-								case STAGE_GLOBAL:
-									throw std::string("qgl::RenderStageComposer: Cannot run STAGE_GLOBAL for new camera");
-									break;
-								case STAGE_PER_CAMERA:
-									stages.emplace_back(s, c);
-									break;
-								case STAGE_PER_CAMERA_FBO:
-									throw std::string("qgl::RenderStageComposer: Cannot run STAGE_PER_CAMERA_FBO for new camera when last stage was STAGE_PER_CAMERA");
-									break;
-							}
-						}
-					}
-				}
-				auto end = std::chrono::steady_clock::now();
-				uint64_t t = (end-begin).count();
-				sumTimeCount += t;
-				timings.push_back({p.stage->pipeline, p.stage, p.camera, t});
-			} else {
-				stages.emplace_back(p);
-			}
-		}
-		std::swap(stages, currentStagesPerCamera);
-		return ret;
-	}
-	
-	bool RenderStageComposer::ContinueStagesPerFbo() {
-		bool ret = false;
-		std::unordered_map<std::shared_ptr<Camera>,
-			std::vector<std::shared_ptr<Stage>>> camerasInUse;
-		{
-			std::vector<StageCameraPair> stages = currentStagesPerFbo;
-			for(auto& p : stages) {
-				if(p.stage->canBeContinued(p.camera)) {
-					ret = true;
-					camerasInUse[p.camera].emplace_back(p.stage);
-				} else {
-					currentStagesPerFbo.emplace_back(p);
+					hasAnyStagesLeft |= s.HasMoreStages();
+					timings.back().End();
 				}
 			}
 		}
-		currentStagesPerFbo.clear();
-		for(auto& cam : camerasInUse) {
-			auto camera = cam.first;
-			camera->UseFbo();
-			for(auto& stage : cam.second) {
-				auto begin = std::chrono::steady_clock::now();
-				stage->renderFunction(camera);
-				auto next = stage->nextStage;
-				if(next) {
-					switch(next->stageType) {
-						case STAGE_GLOBAL:
-							throw std::string("qgl::RenderStageComposer: Cannot run STAGE_GLOBAL after STAGE_PER_CAMERA_FBO");
-							break;
-						case STAGE_PER_CAMERA:
-							throw std::string("qgl::RenderStageComposer: Cannot run STAGE_PER_CAMERA after STAGE_PER_CAMERA_FBO");
-							break;
-						case STAGE_PER_CAMERA_FBO:
-							currentStagesPerFbo.emplace_back(next, camera);
-							break;
-					}
-				} else {
-					auto c = camerasRenderingOrder[camera];
-					if(c) {
-						auto s = firstStageForCameraPerPipeline[stage->pipeline];
-						if(s != nullptr) {
-							switch(s->stageType) {
-								case STAGE_GLOBAL:
-									throw std::string("qgl::RenderStageComposer: Cannot run STAGE_GLOBAL for new camera");
-									break;
-								case STAGE_PER_CAMERA:
-									currentStagesPerCamera.emplace_back(s, c);
-									break;
-								case STAGE_PER_CAMERA_FBO:
-									currentStagesPerFbo.emplace_back(s, c);
-									break;
-							}
-						}
-					}
-				}
-				auto end = std::chrono::steady_clock::now();
-				uint64_t t = (end-begin).count();
-				sumTimeCount += t;
-				timings.push_back({stage->pipeline, stage, camera, t});
-			}
-		}
-		return ret;
+		auto end = std::chrono::steady_clock::now();
+		totalCpuTime +=
+				std::chrono::duration_cast<
+					std::chrono::duration<double>>(
+							end - start).count();
+		return executedAny;
 	}
 	
 	bool RenderStageComposer::HasAnyStagesLeft() {
-		return currentStagesGlobal.size() || currentStagesPerCamera.size() ||
-			currentStagesPerFbo.size();
+		return hasAnyStagesLeft;
 	}
 	
-	void RenderStageComposer::Clear() {
-		timings.clear();
-		
-		camerasRenderingOrder.clear();
-		
-		currentStagesGlobal.clear();
-		currentStagesPerCamera.clear();
-		currentStagesPerFbo.clear();
-		
-		firstStageForCameraPerPipeline.clear();
-		
-		stagingPipelineTemplate.clear();
+	bool RenderStageComposer::CanExecuteSyncStage(uint32_t cameraId,
+			StageOrder stage) {
+		for(auto p : pipelines) {
+			auto s = p->GetStageScheduler().GetNextStage();
+			if(s) {
+				if(s->executionPolicy < STAGE_CAMERA) {
+					return false;
+				}
+				if(p->GetStageScheduler().currentCameraId < cameraId) {
+					return false;
+				} else if(p->GetStageScheduler().currentCameraId == cameraId) {
+					if(s->executionPolicy < stage) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
 	}
 	
-	void RenderStageComposer::PrintStagesStructure() {
-		printf("currentStagesGlobal:\n");
-		for(auto e : currentStagesGlobal) {
-			printf("    %i  <>  %s : %s\n",
-					e->stageType,
-					e->pipeline->GetPipelineName().c_str(),
-					e->stageName.c_str());
+	std::shared_ptr<Camera> RenderStageComposer::GetCameraByIndex(uint32_t id) {
+		if(id < cameras.size()) {
+			return cameras[id];
 		}
-		printf("currentStagesPerCamera:\n");
-		for(auto e : currentStagesPerCamera) {
-			printf("    %p   %i  <>  %s : %s\n",
-					(void*)e.camera.get(),
-					e.stage->stageType,
-					e.stage->pipeline->GetPipelineName().c_str(),
-					e.stage->stageName.c_str());
-		}
-		printf("currentStagesPerFbo:\n");
-		for(auto e : currentStagesPerFbo) {
-			printf("    %p   %i  <>  %s : %s\n",
-					(void*)e.camera.get(),
-					e.stage->stageType,
-					e.stage->pipeline->GetPipelineName().c_str(),
-					e.stage->stageName.c_str());
-		}
+		return nullptr;
 	}
 	
 	void RenderStageComposer::SetGlFinishInEveryStageToProfile(bool value) {
 		enableGlFinishInEveryStageToProfile = value;
+	}
+	
+	std::vector<StageTiming> RenderStageComposer::GetTimings() const {
+		return timings;
+	}
+	
+	double RenderStageComposer::GetTotalCpuTime() const {
+		return totalCpuTime;
 	}
 }
 
